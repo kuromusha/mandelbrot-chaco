aa5803dcbb947b37a0520b4942d1918f7a3672f0
Ken'ichi Kuromusha <kuromusha@users.noreply.github.com>
2023-04-28 10:08:53 +0900

    Change detail level adaptively (#4)
    * change detail level adaptively
    
    * speed up drawing
    
    * fix the calcuration of detail level

--- mandelbrot_chaco.py
+++ mandelbrot_chaco.py
@@ -1,9 +1,10 @@
+import math
 import numpy as np
 
 from chaco.api import ArrayPlotData, Plot
 from chaco.tools.api import BetterSelectingZoom, PanTool
 from enable.api import ComponentEditor
-from traits.api import Array, Float, HasStrictTraits, Property
+from traits.api import Array, Float, HasStrictTraits, Instance, observe
 from traitsui.api import HGroup, UItem, View
 
 from mandel import generate_mandelbrot
@@ -50,16 +51,28 @@ class MandelbrotBetterSelectingZoom(BetterSelectingZoom):
 
 class MandelbrotPlot(HasStrictTraits):
     ranges = Array(dtype=Float, value=[-2, 1, -1.5, 1.5])
-    plot = Property(depends_on='ranges')
+    plot = Instance(Plot)
 
-    def _get_plot(self):
+    def _get_mandelbrot(self):
         x = np.linspace(self.ranges[0], self.ranges[1], num=1000)
         y = np.linspace(self.ranges[2], self.ranges[3], num=1000)
-        plot = Plot(ArrayPlotData(data=generate_mandelbrot(x[:-1], y[:-1], 100)))
+        min_range = min(self.ranges[1] - self.ranges[0], self.ranges[3] - self.ranges[2])
+        n = max(int(math.log10(3 / min_range) * 300 + 100), 1)
+        return x, y, generate_mandelbrot(x[:-1], y[:-1], n)
+
+    def _plot_default(self):
+        x, y, data = self._get_mandelbrot()
+        plot = Plot(ArrayPlotData(data=data))
         plot.img_plot('data', xbounds=x, ybounds=y)
         plot.tools.append(MandelbrotPanTool(self, plot))
         plot.overlays.append(MandelbrotBetterSelectingZoom(self, plot, zoom_factor=1.05))
         return plot
+
+    @observe('ranges')
+    def _plot_update(self, event):
+        x, y, data = self._get_mandelbrot()
+        self.plot.data.set_data('data', new_data=data)
+        self.plot.img_plot('data', xbounds=x, ybounds=y)
 
     traits_view = View(
         HGroup(UItem('plot', editor=ComponentEditor())),
